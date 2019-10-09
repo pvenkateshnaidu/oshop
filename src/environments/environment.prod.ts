64dbe6c7c2cdb0816e04b2516eb7e75e208d0ccc
venky <pvenkateshnaidu@gmail.com>
2019-10-09 20:38:47 +0530

    Initial app

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: true,
+  firebase: {
+   /* apiKey: "AIzaSyAz-Cry0SE3rVMzmUSShP4ANhg_U45CpPk",
+    authDomain: "grocery-webmobilez-usa.firebaseapp.com",
+    databaseURL: "https://grocery-webmobilez-usa.firebaseio.com",
+    projectId: "grocery-webmobilez-usa",
+    storageBucket: "grocery-webmobilez-usa.appspot.com",
+    messagingSenderId: "62339543657",
+    appId: "1:62339543657:web:8188cb1d02b421709e68f8" */
+    apiKey: "AIzaSyBvaoxH3E2d2APU-wfeVL8eB2gBWG8vRgQ",
+    authDomain: "cloudstore-f310f.firebaseapp.com",
+    databaseURL: "https://cloudstore-f310f.firebaseio.com",
+    projectId: "cloudstore-f310f",
+    storageBucket: "cloudstore-f310f.appspot.com",
+    messagingSenderId: "873072926210",
+    appId: "1:873072926210:web:40dacbe6c98c1a1be0511b",
+    measurementId: "G-N6M0652HD5"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    /*  apiKey: "AIzaSyAz-Cry0SE3rVMzmUSShP4ANhg_U45CpPk",
+        authDomain: "grocery-webmobilez-usa.firebaseapp.com",
+        databaseURL: "https://grocery-webmobilez-usa.firebaseio.com",
+        projectId: "grocery-webmobilez-usa",
+        storageBucket: "grocery-webmobilez-usa.appspot.com",
+        messagingSenderId: "62339543657",
+        appId: "1:62339543657:web:8188cb1d02b421709e68f8"        
+    */
+    apiKey: "AIzaSyBvaoxH3E2d2APU-wfeVL8eB2gBWG8vRgQ",
+    authDomain: "cloudstore-f310f.firebaseapp.com",
+    databaseURL: "https://cloudstore-f310f.firebaseio.com",
+    projectId: "cloudstore-f310f",
+    storageBucket: "cloudstore-f310f.appspot.com",
+    messagingSenderId: "873072926210",
+    appId: "1:873072926210:web:40dacbe6c98c1a1be0511b",
+    measurementId: "G-N6M0652HD5"
+  }
+};
